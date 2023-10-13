24f4f8cc5f5662aba6ea76d49438d2e59db61117
Erkan_filiz <erkanfiliz1715@gmail.com>
2023-10-13 22:07:26 +0200

    LifeCycle-react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import LifeCycleMethods from './componants/LifeCycleMethods';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <h1 color="danger"> Life Cycle</h1>
+      <LifeCycleMethods/>
+     
     </div>
   );
 }
